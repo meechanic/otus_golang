fb99e1347cf046f3ef7d894afa42aaa31d24e5e8
Mikhail Efremov <maefremov@avito.ru>
2023-04-02 01:33:32 +0300

    HW1 semms to be completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	const greeting string = "Hello, OTUS!"
+	fmt.Println(stringutil.Reverse(greeting))
 }
