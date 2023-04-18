9422cd3d8ddd483b903176876d0260372ff0f6b7
Mikhail Efremov <maefremov@avito.ru>
2023-04-18 22:00:17 +0300

    Check length is 0 before convertation to runes; also use unicode.IsDigit

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,26 +4,21 @@ import (
 	"errors"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"golang.org/x/example/stringutil"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-// isLetterDigit wraps strconv.Atoi to be more usable in conditionals for checking is rune digit.
-func isRuneDigit(letter rune) bool {
-	_, err := strconv.Atoi(string(letter))
-	return err == nil
-}
-
 // Unpack unpacks strings according to task setting.
 func Unpack(s string) (string, error) {
 	var b strings.Builder
-	r := []rune(s) // Because unicode runes may occur in the input, not just bytes.
-	if len(r) == 0 {
+	if len(s) == 0 {
 		return "", nil
 	}
-	if isRuneDigit(r[0]) {
+	r := []rune(s) // Because unicode runes may occur in the input, not just bytes.
+	if unicode.IsDigit(r[0]) {
 		return "", ErrInvalidString
 	}
 	if len(r) == 1 {
@@ -34,7 +29,7 @@ func Unpack(s string) (string, error) {
 	// If r[i-1] is digit - repeat the rune r[i] as many times as necessary and jump over it.
 	// Else - write r[i].
 	for i := len(r) - 1; i >= 1; i-- {
-		if isRuneDigit(r[i]) && isRuneDigit(r[i-1]) {
+		if unicode.IsDigit(r[i]) && unicode.IsDigit(r[i-1]) {
 			return "", ErrInvalidString
 		}
 		if num, err := strconv.Atoi(string(r[i])); err == nil {
@@ -45,7 +40,7 @@ func Unpack(s string) (string, error) {
 		}
 	}
 	// During the reverse loop, we didn't write the last character if before that we did not meet a digit. Fixing it.
-	if !isRuneDigit(r[1]) {
+	if !unicode.IsDigit(r[1]) {
 		b.WriteRune(r[0])
 	}
 	return stringutil.Reverse(b.String()), nil // Our b has reverse order. Reverse it again to satisfy the task statement.
